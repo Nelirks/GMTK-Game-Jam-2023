d7c3af3de4cf0afbad98745ddcd330a4e6e48b50
raphael lesbros <raphael.lesbros@gmail.com>
2023-07-08 09:01:46 +0200

    Fixed CreateAssetMenu for DialogueInfo

--- Assets/Scripts/DialogueBox/DialogueInfo.cs
+++ Assets/Scripts/DialogueBox/DialogueInfo.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/DialogueLine", order = 1)]
+
 public class DialogueInfo : ScriptableObject
 {
     public List<DialogueLine> dialogueLines;
