307045599755e9a85a920a34f02c264cf236969f
Adil Chaouch <77068496+lsadil@users.noreply.github.com>
2023-07-09 00:13:45 +0200

    Added Pnjs near the bar

--- Assets/Scripts/PnjController.cs
+++ Assets/Scripts/PnjController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PnjController : MonoBehaviour
 {
@@ -10,12 +11,30 @@ public class PnjController : MonoBehaviour
 
     private int currentWaypointIndex = 0;
     private bool isPaused = false;
+    private Rigidbody2D rb;
+    private SpriteRenderer spriteRenderer;
+
 
     private void Start()
     {
         StartCoroutine(MoveToNextWaypoint());
+        rb = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
+    private void FixedUpdate()
+    {
+        Debug.Log(rb.velocity);
+        if (rb.velocity.x > 0)
+        {
+            spriteRenderer.flipX = true;
+        }
+        else if (rb.velocity.x < 0)
+        {
+            spriteRenderer.flipX = false;
+        }
 
+        //not working, MoveTowards doesn't change velocity
+    }
     private IEnumerator MoveToNextWaypoint()
     {
         while (true)
