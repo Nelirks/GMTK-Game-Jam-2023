344a229cbc856db231b13b85332d5071e891bf82
Adil Chaouch <77068496+lsadil@users.noreply.github.com>
2023-07-09 01:26:20 +0200

    Audio mixers integration

--- Assets/Scripts/PickableObject.cs
+++ Assets/Scripts/PickableObject.cs
@@ -35,7 +35,6 @@ public class PickableObject : MonoBehaviour
         OnInventoryAdd();
         playerLogic.Craft(this, craftableWith, craftResult);
         gameObject.SetActive(false);
-
     }
 
     public void UseObject()
